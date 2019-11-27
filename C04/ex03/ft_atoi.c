632e275775fec8ba3dc4c9d1641b629d4a091c20
Juancar <jserrano231@gmail.com>
2019-11-27 12:13:16 +0000

    ft_atoi, pending test

---
+++ C04/ex03/ft_atoi.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+
+int	ft_atoi(char *str)
+{
+	int x;
+	int num;
+	int sign;
+
+	x = 0;
+	num = 0;
+	sign = 1;
+	while ((str[x] > 10 && str[x] < 14) || (str[x] == 32))
+		++x;
+	if (str[x] == '+' || str[x] == '-')
+	{
+		if (str[x] == '-')
+			sign = -1;
+		++x;
+	}
+	if (str[x] > 47 || str[x] < 58)
+	{
+		while (str[x] != '\0')
+		{
+		num = (num * 10) + (str[x] - '0');
+		write(1, &num, x);
+		++x;
+		}
+	num *= sign;
+	}
+	return (num);
+}
+
+int	main(void)
+{
+	ft_atoi(" +-1490");
+	return (0);
+}
