49e42aee63491c16fc637627f55e999ea75d5b5c
Juancar <jserrano231@gmail.com>
2019-12-05 17:30:49 +0000

    Corrected next_prime

--- C05/ex07/ft_find_next_prime.c
+++ C05/ex07/ft_find_next_prime.c
@@ -13,26 +13,28 @@ int     ft_is_prime(int nb)
 			return (0);
 		++x;
 	}
-	return (nb);
+	return (1);
 }
 
 int	ft_find_next_prime(int nb)
 {
-	int next;
 	int check;
-	
-	next = nb + 1;
-	check = ft_is_prime(next);	
+	int next;
+
+	check = ft_is_prime(nb);
 	if (check == 0)
-		ft_find_next_prime(nb + 1);
-	return (check);
+	{
+		next = ft_find_next_prime(nb + 1);
+		return(next);
+	}
+	return (nb);
 }
 
 int	main(void)
 {
 	int z;
 
-	z = ft_find_next_prime(7);
-	printf("El siguiente numero primo es %d\n", z);
+	z = ft_find_next_prime(114);
+	printf("El numero primo es %d\n", z);
 	return (0);
 }
